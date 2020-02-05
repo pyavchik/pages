862fe39bdfa1281390798f4b5ce54191f05cdec6
pyavchik <pyavchik@gmail.com>
2020-02-05 19:34:26 +0200

    ng-containger directive added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,6 +27,230 @@ export class AppComponent {
       title: 'Moon',
       // tslint:disable-next-line:max-line-length
       url: 'https://images.unsplash.com/photo-1572925077991-61acf7d70608?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'At the Beach',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'Nice photo',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1548984363-d6e54c098bc5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'Old fox',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1509618635931-82c44053a9ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'Moon',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1572925077991-61acf7d70608?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'At the Beach',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'Nice photo',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1548984363-d6e54c098bc5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'Old fox',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1509618635931-82c44053a9ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'Moon',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1572925077991-61acf7d70608?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'At the Beach',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'Nice photo',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1548984363-d6e54c098bc5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'Old fox',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1509618635931-82c44053a9ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'Moon',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1572925077991-61acf7d70608?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'At the Beach',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'Nice photo',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1548984363-d6e54c098bc5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'Old fox',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1509618635931-82c44053a9ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'Moon',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1572925077991-61acf7d70608?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'At the Beach',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'Nice photo',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1548984363-d6e54c098bc5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'Old fox',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1509618635931-82c44053a9ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'Moon',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1572925077991-61acf7d70608?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'At the Beach',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'Nice photo',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1548984363-d6e54c098bc5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'Old fox',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1509618635931-82c44053a9ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'Moon',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1572925077991-61acf7d70608?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'At the Beach',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'Nice photo',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1548984363-d6e54c098bc5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'Old fox',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1509618635931-82c44053a9ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'Moon',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1572925077991-61acf7d70608?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'At the Beach',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'Nice photo',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1548984363-d6e54c098bc5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'Old fox',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1509618635931-82c44053a9ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'Moon',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1572925077991-61acf7d70608?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'At the Beach',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'Nice photo',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1548984363-d6e54c098bc5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'Old fox',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1509618635931-82c44053a9ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'Moon',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1572925077991-61acf7d70608?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'At the Beach',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'Nice photo',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1548984363-d6e54c098bc5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'Old fox',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1509618635931-82c44053a9ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'Moon',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1572925077991-61acf7d70608?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'At the Beach',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'Nice photo',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1548984363-d6e54c098bc5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'Old fox',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1509618635931-82c44053a9ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'Moon',
+      // tslint:disable-next-line:max-line-length
+      url: 'https://images.unsplash.com/photo-1572925077991-61acf7d70608?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
     }
   ];
+
+  checkWindowIndex(index: number) {
+    return Math.abs(this.currentPage - index) < 5;
+  }
 }
